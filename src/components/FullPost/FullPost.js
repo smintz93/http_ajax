1615d01d797f16c9842610832794109045bfdcb1
Sam <sammintz1993@gmail.com>
2018-11-05 08:14:01 -0600

    only get one request per get request

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -9,18 +9,20 @@ class FullPost extends Component {
     }
     componentDidUpdate(){
         if(this.props.id) {
-            axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
-                .then(response => {
-                    // console.log(response);
-                    this.setState({loadedPost: response.data})
-                })
-        }
+            if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                    .then(response => {
+                        // console.log(response);
+                        this.setState({loadedPost: response.data})
+                    })
+                }
+            }    
        
     }
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
         if(this.props.id) {
-            post = <p style={{textAlign: 'center'}}>Loading</p>;
+            post = <p style={{textAlign: 'center'}}>Loading...</p>;
         }
         if (this.state.loadedPost) {
              post = (
