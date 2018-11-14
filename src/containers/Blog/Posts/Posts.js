5343933d69d300e3c35154a5bad6e5a329e59ac5
Sam <sammintz1993@gmail.com>
2018-11-14 09:33:59 -0600

    changed Link to NavLink to determine active page

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -11,6 +11,7 @@ class Posts extends Component {
     }
 
     componentDidMount(){
+    	console.log(this.props)
         // es6 promises 
         axios.get('/posts')
             .then((response) => {
