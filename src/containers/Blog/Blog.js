99b27008d2e1b7d7c24333cc95e8448d5e15cc9c
Sam <sammintz1993@gmail.com>
2018-11-14 09:16:21 -0600

    removed a tags from Link from React Router

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts'
+import NewPost from './NewPost/NewPost'
 
 class Blog extends Component {
     
@@ -14,13 +15,17 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to={{
+                                pathname: "/new-post",
+                                hash: "#submit"
+                            }}>New Post</Link></li>
                             
                         </ul>
                     </nav>
                 </header>
                <Route path="/" exact component={Posts} />
+               <Route path="/new-post" component={NewPost} />
             </div>
         );
     }
