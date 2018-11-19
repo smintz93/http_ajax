bb803307a0b9fe5b0a99a9e18d582486bd957d2e
Sam <sammintz1993@gmail.com>
2018-11-19 08:57:49 -0600

    switch component to only show one route at a time

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts'
@@ -26,9 +26,12 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-               <Route path="/" exact component={Posts} />
-               <Route path="/new-post" component={NewPost} />
-               <Route path="/:id" exact component={FullPost} />
+                <Switch>
+                    <Route path="/" exact component={Posts} />
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/:id" exact component={FullPost} />
+                </Switch>
+           
              
             </div>
         );
