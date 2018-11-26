326034349784018f8851b14da0e8c7d1f077341a
Sam <sammintz1993@gmail.com>
2018-11-26 09:30:51 -0600

    asyncComponent to only load data from the component user is on

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,7 +4,12 @@ import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+// import NewPost from './NewPost/NewPost';
+const AsyncNewPost = asyncComponent(() => {
+    // dynamic import function 
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
     state = {
@@ -28,7 +33,7 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null }
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null }
                     <Route path="/posts"  component={Posts} />
                     <Route render={() => <h1>Not found</h1> } /> 
                    {/* <Redirect from="/" to="/posts" /> */}
